e246afbb74eed7cee19f99c934bc0ee88c8f5d99
Manuel Popp <34488159+ManuelPopp@users.noreply.github.com>
2021-09-25 13:44:39 +0200

    Add files via upload

---
+++ split_jobs/Run_Rscript.bat
@@ -0,0 +1,2 @@
+"C:\Program Files\R\R-4.0.2\bin\Rscript.exe" "D:\Dateien\Studium_KIT\Master_GOEK\10_FS_Geooekologie\Multi-skalige_Fernerkundungsverfahren\Projekt\rsc\split_jobs\Parallel_job3.R" 1 1
+pause
