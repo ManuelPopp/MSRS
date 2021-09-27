05188db32ee20ce5435ef312eefa8e203854145c
Manuel Popp <34488159+ManuelPopp@users.noreply.github.com>
2021-09-27 17:28:52 +0200

    Update Parallel_job5.R

--- split_jobs/Parallel_job5.R
+++ split_jobs/Parallel_job5.R
@@ -13,7 +13,7 @@ if(length(args) > 1){
 #######################################################################|
 #----------------------------------------------------------------------|
 # general settings
-packages <- c("raster", "sf", "rgeos", "rgdal", "caret", "e1071")
+packages <- c("raster", "sf", "rgeos", "rgdal", "stringr")
 for(i in 1:NROW(packages)){
   if(!require(packages[i], character.only = TRUE)){
     install.packages(packages[i])
@@ -22,7 +22,14 @@ for(i in 1:NROW(packages)){
 }
 wd <- "G:/Projekt"
 lc_classified_path <- file.path(wd, "out", "lcc")
-filenames <- list.files(path = lc_classified_path, pattern = ".tif")
+
+#----------------------------------------------------------------------|
+## select spatial grain in m
+spatial_grains <- c(0.4, 1, 2, 4, 10, 20)
+grain <- spatial_grains[as.numeric(args[1])]
+
+filenames <- list.files(path = lc_classified_path,
+                        pattern = paste0("Grn_", grain, "_.*\\.tif$"))
 
 #----------------------------------------------------------------------|
 ## start progress bar
@@ -30,14 +37,15 @@ pb = txtProgressBar(min = 0, max = length(filenames), initial = 0)
 step_i <- 1
 for(scene in filenames){
   setTxtProgressBar(pb, step_i)
-  file <- file.path(lc_classified_path, paste("Grn", grain,
-                                              scene, sep = "_"))
+  file <- file.path(lc_classified_path, scene)
   rst <- raster::raster(file)
   # calculate frequencies per class
   cfreq <- raster::freq(rst)
   cfreq <- data.frame(cfreq)
   sum_pxls <- sum(cfreq[, 2])
-  df <- data.frame(tile = rep(str_replace(scene, ".tif", ""),
+  tile_no <- str_replace(str_replace(scene, ".tif", ""),
+                         paste0("Grn_", grain, "_"), "")
+  df <- data.frame(tile = rep(tile_no,
                               nrow(cfreq)),
                    sp_grain = rep(grain, nrow(cfreq)),
                    class = cfreq[, 1],
